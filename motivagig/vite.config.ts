ce8d0c31809c675d81e7b979d6c0a261eefb4bb2
phestalise <phestalise@gmail.com>
2025-11-16 14:40:50 -0300

    faet:ajustando vite

--- motivagig/vite.config.ts
+++ motivagig/vite.config.ts
@@ -2,23 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", 
-  build: {
-    sourcemap: true, 
-    minify: false,
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
-  },
-  server: {
-    port: 5173, 
-    open: true,
-  },
 });
