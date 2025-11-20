61c266621410a6c960a377ae25992a6a44cc0043
emanuel-italo <emanuelitaloleal@hotmail.com>
2025-11-20 10:52:27 -0300

    feat:Adicionando carregamento de dados do trabalhador logado no EditarPerfil Implementa useEffect para buscar dados do trabalhador ao montar o componente.

--- motivagig/src/routes/EditarPerfil/index.tsx
+++ motivagig/src/routes/EditarPerfil/index.tsx
@@ -20,3 +20,22 @@ export default function EditarPerfil() {
  
   const API_URL = import.meta.env.VITE_API_URL;
   const API_KEY = import.meta.env.VITE_API_KEY;
+  useEffect(() => {
+    const carregarDados = async () => {
+      try {
+        const dadosSessao = sessionStorage.getItem("trabalhadorLogado");
+        if (!dadosSessao) {
+          alert("Sessão expirada. Faça login novamente.");
+          return;
+        }
+ 
+        const usuarioLogado = JSON.parse(dadosSessao);
+        const id = usuarioLogado.id;
+ 
+        const response = await fetch(`${API_URL}/${id}`, {
+          method: "GET",
+          headers: {
+            "Accept": "application/json",
+            "X-API-Key": API_KEY,
+          },
+        });
